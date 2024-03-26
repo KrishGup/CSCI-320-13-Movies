e29633b18667b47b2c696d6e0fe802f117bf53ca
sdm9252 <sdm9252@g.rit.edu>
2024-03-26 13:16:28 -0400

    followed coutnt

--- pythontesting/movie_lover_ui.py
+++ pythontesting/movie_lover_ui.py
@@ -8,3 +8,7 @@ def movie_lover_ui(uid, conn):
     curs.execute("select count(*) from follows where followeduid = %s", (uid,))
     followercount = curs.fetchone()[0]
     print("you have "+ str(followercount) + " followers")
+
+    curs.execute("select count(*) from follows where followeruid = %s", (uid,))
+    followedcount = curs.fetchone()[0]
+    print("you follow "+ str(followedcount) + " movie lover(s)")
